77524bcef17b3b8b8503f5b286af6d3c06a1256f
KarmiTenchi <73952811+KarmiTenchi@users.noreply.github.com>
2022-10-29 14:57:01 +0300

    initial commit

---
+++ linktree/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div id="img_container">
+      <img id="profile__img" src="" />
+
+      <div id="img_name">Annette Black</div>
+    </div>
+  );
+}
+
+export default Header;
